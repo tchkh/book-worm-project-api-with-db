5de2e8fd5c2d0f3a76c466f097b0f15f6b06426b
jayOnTheStreet <taychakhun.r@gmail.com>
2025-04-28 16:39:51 +0700

    feat: add post /posts api endpoint

--- app.mjs
+++ app.mjs
@@ -1,5 +1,6 @@
 import express from 'express'
 import cors from 'cors'
+import connectionPool from './utils/db.mjs'
 
 const app = express()
 const port = process.env.PORT || 4001
@@ -7,13 +8,22 @@ const port = process.env.PORT || 4001
 app.use(cors())
 app.use(express.json())
 
-app.get('/profiles', (req, res) => {
-  return res.json({
-    data: {
-      name: 'john',
-      age: 20,
-    },
-  })
+app.post('/posts', async (req, res) => {
+  try {
+    const {
+      body: { title, image, category_id, description, content, status_id },
+    } = req
+
+    const createPost = await connectionPool.query({
+      text: 'INSERT INTO posts (title, image, category_id, description, content, status_id) VALUES ($1, $2, $3, $4, $5, $6) RETURNING *',
+      values: [title, image, category_id, description, content, status_id],
+    })
+
+    return res.status(201).json({ message: 'Created post successfully' })
+  } catch (error) {
+    console.log(error)
+    return res.status(500).json({ error: error.message })
+  }
 })
 
 app.listen(port, () => {
